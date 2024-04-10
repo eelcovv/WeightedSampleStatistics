1fbec0c2d4d803c44101de18d34ba7656a1c0a33
Eelco van Vliet <eelcovv@gmail.com>
2024-04-10 21:33:07 +0200

    fixed unit test

--- tests/test_weighted_sample_statistics.py
+++ tests/test_weighted_sample_statistics.py
@@ -1,4 +1,4 @@
-from weighted_sample_statistics.core import Weightedweighted_sample_statistics
+from weighted_sample_statistics import WeightedSampleStatistics
 
 __author__ = "Eelco van Vliet"
 __copyright__ = "Eelco van Vliet"
@@ -9,7 +9,7 @@ def test_main(capsys):
     """CLI Tests"""
     # capsys is a pytest fixture that allows asserts against stdout/stderr
     # https://docs.pytest.org/en/stable/capture.html
-    weighted_sample_statistics = Weightedweighted_sample_statistics(
+    weighted_sample_statistics = WeightedSampleStatistics(
         group_keys=None, records_df_selection=None, weights_df=None
     )
     assert weighted_sample_statistics.group_keys is None
