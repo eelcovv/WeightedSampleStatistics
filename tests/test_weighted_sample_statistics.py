73de37c2efe67c58d67ddaeda3fa1bdf968b4581
Eelco van Vliet <eelcovv@gmail.com>
2024-04-10 22:21:53 +0200

    updated unit test

--- tests/test_weighted_sample_statistics.py
+++ tests/test_weighted_sample_statistics.py
@@ -1,15 +1,18 @@
 from weighted_sample_statistics import WeightedSampleStatistics
+from weighted_sample_statistics import reorganise_stat_df
 
 __author__ = "Eelco van Vliet"
 __copyright__ = "Eelco van Vliet"
 __license__ = "MIT"
 
 
-def test_main(capsys):
-    """CLI Tests"""
-    # capsys is a pytest fixture that allows asserting against stdout/stderr
-    # https://docs.pytest.org/en/stable/capture.html
+def test_class_import():
     weighted_sample_statistics = WeightedSampleStatistics(
         group_keys=None, records_df_selection=None, weights_df=None
     )
     assert weighted_sample_statistics.group_keys is None
+
+
+def test_reorganise_stat_df():
+    function = reorganise_stat_df
+    assert function is not None
