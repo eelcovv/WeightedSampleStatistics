cc275ea7e44fd48b3d4e04f36b748e08da51f707
EVLT <evlt@cbs.nl>
2024-10-24 15:56:26 +0200

    added quick imports

--- src/weighted_sample_statistics/__init__.py
+++ src/weighted_sample_statistics/__init__.py
@@ -1,10 +1,4 @@
-import sys
-
-if sys.version_info[:2] >= (3, 8):
-    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
-    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
-else:
-    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
+from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
 
 try:
     # Change here if the project is renamed and does not equal the package name
@@ -17,7 +11,6 @@ finally:
 
 from .core import WeightedSampleStatistics as WeightedSampleStatistics
 from .variable_properties import VariableProperties as VariableProperties
-from .utils import rename_all_variables as rename_all_variables
 from .utils import make_negation_name as make_negation_name
 from .utils import get_records_select as get_records_select
 from .utils import prepare_df_for_statistics as prepare_df_for_statistics
