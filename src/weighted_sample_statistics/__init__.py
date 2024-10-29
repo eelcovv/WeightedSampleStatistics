bde968ea7431d4709ef041e741eab8275297ca8f
Eelco van Vliet <eelcovv@gmail.com>
2024-10-29 12:02:49 +0100

    removed all unused functions and modules

--- src/weighted_sample_statistics/__init__.py
+++ src/weighted_sample_statistics/__init__.py
@@ -10,8 +10,4 @@ finally:
     del version, PackageNotFoundError
 
 from .core import WeightedSampleStatistics as WeightedSampleStatistics
-from .variable_properties import VariableProperties as VariableProperties
 from .utils import make_negation_name as make_negation_name
-from .utils import get_records_select as get_records_select
-from .utils import prepare_df_for_statistics as prepare_df_for_statistics
-from .utils import reorganise_stat_df as reorganise_stat_df
